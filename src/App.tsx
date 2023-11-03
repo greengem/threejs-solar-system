17e4efaa74bcdf686301a38200108f5b3a94e699
Green Gem <chris_waitt@me.com>
2023-11-03 14:44:59 +0000

    Add camera controls to move and zoom around the canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css';
 import { Canvas, useLoader, useThree } from '@react-three/fiber';
 import { TextureLoader, EquirectangularReflectionMapping } from 'three';
+import { OrbitControls } from '@react-three/drei';
 import { useEffect } from 'react';
+import Planet from './components/celestial/Planet';
 import Sun from './components/celestial/Sun';
 
 type SceneBackgroundProps = {
@@ -37,11 +39,13 @@ function App() {
   return (
     <div className='w-screen h-screen overflow-hidden'>
       <Canvas>
+        <OrbitControls enableZoom={true} rotateSpeed={0.7} />
         <SceneBackground texturePath={texturePath} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
         <pointLight position={[-10, -10, -10]} />
         <Sun position={[0, 0, 0]} />
+        <Planet position={[5, 0, 0]} color="red" size={1} />
       </Canvas>
     </div>
   );
