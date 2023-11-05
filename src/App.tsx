89bf1527e29ad751b8e807c42db4e910032baefe
Green Gem <chris_waitt@me.com>
2023-11-05 23:21:27 +0000

    Move contents of app to sub component and add context provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,13 @@
-import { useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-
-import CameraController from './components/CameraController';
-import SceneBackground from './components/SceneBackground';
-import Sun from './components/celestial/Sun';
-import Planet from './components/celestial/Planets';
-import planetsData from './Data/planetsData';
-import PlanetMenu from './components/PlanetMenu';
-import { PlanetData } from '../types';
-
+// App.tsx
+import { SelectedPlanetProvider } from './contexts/SelectedPlanetContext';
+import SolarSystem from './components/SolarSystem';
 function App() {
-  const [selectedPlanet, setSelectedPlanet] = useState<PlanetData | null>(null);
-
-  const handlePlanetSelect = (planetName: string) => {
-    const selected = planetsData.find((planet) => planet.name === planetName);
-    if (selected) {
-      setSelectedPlanet(selected);
-    } else {
-      setSelectedPlanet(null);
-    }
-  };
-
   return (
-    <div className='w-screen h-screen overflow-hidden'>
-      <Canvas>
-        <CameraController selectedPlanet={selectedPlanet} />
-        <SceneBackground texturePath="/images/background/stars_8k.jpg" />
-        <ambientLight intensity={0.2} />
-        <directionalLight intensity={1} position={[2, 2, 2]} target-position={[0, 0, 0]} />
-        <pointLight position={[0, 0, 0]} intensity={1.5} decay={2} distance={10} />
-        <pointLight position={[10, 10, 10]} intensity={0.5} />
-        <pointLight position={[-10, -10, -10]} intensity={0.5} />
-        <Sun position={[0, 0, 0]} radius={1} />
-        {planetsData.map((planet) => (
-          <Planet
-            key={planet.name}
-            name={planet.name}
-            texturePath={planet.texturePath}
-            position={planet.position}
-            radius={planet.radius}
-            rotationSpeed={planet.rotationSpeed}
-            tilt={planet.tilt}
-            orbitSpeed={planet.orbitSpeed}
-            moons={planet.moons}
-            wobble={planet.wobble}
-            rings={planet.rings}
-        />
-        
-        ))}
-      </Canvas>
-      <PlanetMenu planets={planetsData} onSelect={handlePlanetSelect} />
-    </div>
+    <SelectedPlanetProvider>
+      <div className='w-screen h-screen overflow-hidden'>
+        <SolarSystem />
+      </div>
+    </SelectedPlanetProvider>
   );
 }
 
---
+++ src/components/SolarSystem.tsx
@@ -0,0 +1,59 @@
+// SolarSystem.tsx
+import { useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+
+import CameraController from './CameraController';
+import SceneBackground from './SceneBackground';
+import Sun from './celestial/Sun';
+import planetsData from '../Data/planetsData';
+import Planet from './celestial/Planets';
+import PlanetMenu from './PlanetMenu';
+import PlanetsUpdater from './PlanetsUpdater';
+import { useSelectedPlanet } from '../contexts/SelectedPlanetContext'; // Make sure this path is correct
+
+function SolarSystem() {
+  const [selectedPlanet] = useSelectedPlanet(); // Use the context to get the selected planet
+  
+  // State to handle the angles of the planets for their orbits
+  const [planetAngles, setPlanetAngles] = useState<{ [key: string]: number }>(
+    planetsData.reduce((acc, planet) => {
+      acc[planet.name] = 0;
+      return acc;
+    }, {})
+  );
+
+  return (
+    <>
+      <Canvas>
+        <CameraController selectedPlanet={selectedPlanet} />
+        <SceneBackground texturePath="/images/background/stars_8k.jpg" />
+        <ambientLight intensity={0.2} />
+        <directionalLight intensity={1} position={[2, 2, 2]} target-position={[0, 0, 0]} />
+        <pointLight position={[0, 0, 0]} intensity={1.5} decay={2} distance={10} />
+        <pointLight position={[10, 10, 10]} intensity={0.5} />
+        <pointLight position={[-10, -10, -10]} intensity={0.5} />
+        <Sun position={[0, 0, 0]} radius={1} />
+        {planetsData.map((planet) => (
+          <Planet
+            key={planet.name}
+            name={planet.name}
+            texturePath={planet.texturePath}
+            position={planet.position}
+            radius={planet.radius}
+            rotationSpeed={planet.rotationSpeed}
+            tilt={planet.tilt}
+            orbitSpeed={planet.orbitSpeed}
+            moons={planet.moons}
+            wobble={planet.wobble}
+            rings={planet.rings}
+            angle={planetAngles[planet.name]}
+          />
+        ))}
+        <PlanetsUpdater setPlanetAngles={setPlanetAngles} />
+      </Canvas>
+      <PlanetMenu planets={planetsData} />
+    </>
+  );
+}
+
+export default SolarSystem;
