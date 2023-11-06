a8d76f9dfbd07ec232e87e21e4ae16e401291484
Green Gem <chris_waitt@me.com>
2023-11-06 10:40:53 +0000

    Install ui library for fancy sliders

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 // App.tsx
-import { SelectedPlanetProvider } from './contexts/SelectedPlanetContext';
 import SolarSystem from './components/SolarSystem';
+import { SelectedPlanetProvider } from './contexts/SelectedPlanetContext';
+import { SpeedControlProvider } from './contexts/SpeedControlContext';
+import {NextUIProvider} from "@nextui-org/react";
+
 function App() {
   return (
-    <SelectedPlanetProvider>
-      <div className='w-screen h-screen overflow-hidden'>
-        <SolarSystem />
-      </div>
-    </SelectedPlanetProvider>
+    <NextUIProvider>
+      <SelectedPlanetProvider>
+        <SpeedControlProvider>
+          <div className='w-screen h-screen overflow-hidden'>
+            <SolarSystem />
+          </div>
+        </SpeedControlProvider>
+      </SelectedPlanetProvider>
+    </NextUIProvider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 
