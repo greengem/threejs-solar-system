6cf171a54b4abe4a0529f28a28f9533244c7e1f1
Green Gem <chris_waitt@me.com>
2023-11-06 20:48:18 +0000

    Add description to planet info and set speed to 0 while in detail view

--- src/components/PlanetDetail.tsx
+++ src/components/PlanetDetail.tsx
@@ -1,27 +1,26 @@
 // PlanetDetail.tsx
-import { Button } from '@nextui-org/react';
+import { Button, CardHeader } from '@nextui-org/react';
 import { useSelectedPlanet } from '../contexts/SelectedPlanetContext';
-//import { useSpeedControl } from '../contexts/SpeedControlContext';
+import { useSpeedControl } from '../contexts/SpeedControlContext';
 import { Card, CardBody } from '@nextui-org/react';
 
 const PlanetDetail: React.FC = () => {
   const [selectedPlanet, setSelectedPlanet] = useSelectedPlanet();
-  //const { setSpeedFactor } = useSpeedControl();
+  const { setSpeedFactor } = useSpeedControl();
 
   const handleExitDetailMode = () => {
     setSelectedPlanet(null);
-    //setSpeedFactor(1); // Restore the speed to its previous value or a default one
+    setSpeedFactor(1);
   };
 
-  // Render nothing if no planet is selected
   if (!selectedPlanet) return null;
 
-  // If a planet is selected, render the detail card
   return (
-    <Card className='absolute bottom-5 right-5 w-[300px] h-[300px] bg-gray-900 shadow-lg'>
-      <CardBody>
-        <h1 className='text-lg font-semibold tracking-tight uppercase text-white'>{selectedPlanet.name}</h1>
-        <Button color='secondary' className='absolute bottom-5 left-5' onClick={handleExitDetailMode}>
+    <Card className='absolute bottom-5 right-5 w-[300px] bg-gray-900 shadow-lg'>
+      <CardHeader className='text-lg font-semibold tracking-tight uppercase text-white px-5'>{selectedPlanet.name}</CardHeader>
+      <CardBody className='px-5'>
+        <p className='text-gray-500 text-sm mb-5'>{selectedPlanet.description}</p>
+        <Button color='secondary' onClick={handleExitDetailMode}>
           Exit Detail View
         </Button>
       </CardBody>
--- src/components/PlanetMenu.tsx
+++ src/components/PlanetMenu.tsx
@@ -1,6 +1,6 @@
 // PlanetMenu.tsx
 import React from 'react';
-//import { useSpeedControl } from '../contexts/SpeedControlContext';
+import { useSpeedControl } from '../contexts/SpeedControlContext';
 import { useSelectedPlanet } from '../contexts/SelectedPlanetContext';
 import { PlanetData } from '../../types';
 import { Button } from '@nextui-org/react';
@@ -11,19 +11,27 @@ interface PlanetMenuProps {
 
 const PlanetMenu: React.FC<PlanetMenuProps> = ({ planets }) => {
   const [, setSelectedPlanet] = useSelectedPlanet();
-  //const { setSpeedFactor } = useSpeedControl();
-  
+  const { setSpeedFactor } = useSpeedControl();
+
   const handleSelect = (planetName: string) => {
     const selected = planets.find((planet) => planet.name === planetName);
     setSelectedPlanet(selected ?? null);
-    //setSpeedFactor(0);
+    setSpeedFactor(0);
   };
 
   return (
     <div className="absolute bottom-5 left-5 p-2 rounded-lg">
       <div className='flex gap-2 flex-wrap'>
         {planets.map((planet) => (
-            <Button key={planet.name} variant='ghost' color='secondary' size='sm' onClick={() => handleSelect(planet.name)}>{planet.name}</Button>
+          <Button 
+            key={planet.name} 
+            variant='ghost' 
+            color='secondary' 
+            size='sm' 
+            onClick={() => handleSelect(planet.name)}
+          >
+            {planet.name}
+          </Button>
         ))}
       </div>
     </div>
