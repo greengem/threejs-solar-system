95a6791874c35b0982fa2b917ee26403f5be9c16
Green Gem <chris_waitt@me.com>
2023-11-06 14:27:18 +0000

    tweak speed control slider UI

--- src/components/SpeedControl.tsx
+++ src/components/SpeedControl.tsx
@@ -9,13 +9,14 @@ const SpeedControl = () => {
     <div className='absolute top-5 right-5'>
         <Slider
             size="lg"
+            label="Speed Control"
             step={0.01}
             maxValue={5}
-            minValue={0.1}
+            minValue={0}
             aria-label="Speed control"
             defaultValue={speedFactor}
             onChange={(value) => setSpeedFactor(Number(value))}
-            className="w-[300px]"
+            className="w-[300px] text-gray-200"
             color='secondary'
         />
     </div>
