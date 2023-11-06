43cec618ca819ddcb1534c01117c668eb3504d45
Green Gem <chris_waitt@me.com>
2023-11-06 10:41:10 +0000

    use new buttons for seelcting planets

--- src/components/PlanetMenu.tsx
+++ src/components/PlanetMenu.tsx
@@ -1,30 +1,28 @@
 // PlanetMenu.tsx
 import React from 'react';
-import { useSelectedPlanet } from '../contexts/SelectedPlanetContext'; // Update the path as needed
+import { useSelectedPlanet } from '../contexts/SelectedPlanetContext';
 import { PlanetData } from '../../types';
+import { Button } from '@nextui-org/react';
 
 interface PlanetMenuProps {
   planets: PlanetData[];
 }
 
 const PlanetMenu: React.FC<PlanetMenuProps> = ({ planets }) => {
-  const [, setSelectedPlanet] = useSelectedPlanet(); // We use only the setter function from the context
+  const [, setSelectedPlanet] = useSelectedPlanet();
 
   const handleSelect = (planetName: string) => {
-    // Find the planet data based on the planet name
     const selected = planets.find((planet) => planet.name === planetName);
-    setSelectedPlanet(selected ?? null); // Set the selected planet using context
+    setSelectedPlanet(selected ?? null);
   };
 
   return (
     <div className="planet-menu-overlay bg-gray-900 absolute bottom-5 left-5 right-5 p-2 rounded-lg">
-      <ul className="planet-list flex gap-2">
+      <div className='flex gap-2 flex-wrap'>
         {planets.map((planet) => (
-          <li key={planet.name} onClick={() => handleSelect(planet.name)} className='bg-purple-800 px-4 py-1 rounded-lg text-white text-sm'>
-            {planet.name}
-          </li>
+            <Button key={planet.name} color='secondary' size='sm' onClick={() => handleSelect(planet.name)}>{planet.name}</Button>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
