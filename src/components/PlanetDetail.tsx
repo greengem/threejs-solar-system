ebec2b52187f2138b96313873504d581ab643add
Green Gem <chris_waitt@me.com>
2023-11-06 14:37:40 +0000

    use our selected planet context to add details to the planet detail window

--- src/components/PlanetDetail.tsx
+++ src/components/PlanetDetail.tsx
@@ -1,12 +1,22 @@
 // PlanetDetail.tsx
 import { Button } from '@nextui-org/react';
-//import { PlanetData } from '../../types';
-  
+import { useSelectedPlanet } from '../contexts/SelectedPlanetContext'; // Import the context hook
+
 const PlanetDetail: React.FC = () => {
+  // Use the useSelectedPlanet hook to access the selected planet data
+  const [selectedPlanet] = useSelectedPlanet();
+
   return (
-    <div className="planet-detail bg-gray-900  absolute top-20 bottom-20 right-5 p-5 rounded-lg w-[400px]">
-        <p className='text-white'>Planet Name</p>
-        <Button>Exit Detail View</Button>
+    <div className="planet-detail bg-gray-900 absolute top-20 bottom-20 right-5 p-5 rounded-lg w-[400px]">
+      {/* Check if a planet is selected and display its name */}
+      {selectedPlanet ? (
+        <>
+          <p className='text-white'>{selectedPlanet.name}</p>
+          <Button>Exit Detail View</Button>
+        </>
+      ) : (
+        <p className='text-white'>No planet selected</p>
+      )}
     </div>
   );
 };
