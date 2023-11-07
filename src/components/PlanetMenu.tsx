763c975c5fa3d65fbe5d606aa2abd56bf51eaa97
Green Gem <chris_waitt@me.com>
2023-11-07 16:02:29 +0000

    Disable planet menu buttons during intro animation

--- src/components/PlanetMenu.tsx
+++ src/components/PlanetMenu.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useSpeedControl } from '../contexts/SpeedControlContext';
 import { useSelectedPlanet } from '../contexts/SelectedPlanetContext';
+import { useCameraContext } from '../contexts/CameraContext';
 import { PlanetData } from '../../types';
 import { Button } from '@nextui-org/react';
 
@@ -12,18 +13,23 @@ interface PlanetMenuProps {
 const PlanetMenu: React.FC<PlanetMenuProps> = ({ planets }) => {
   const [, setSelectedPlanet] = useSelectedPlanet();
   const { overrideSpeedFactor } = useSpeedControl();
+  const { cameraState, setCameraState } = useCameraContext();
 
   const handleSelect = (planetName: string) => {
     const selected = planets.find((planet) => planet.name === planetName);
     setSelectedPlanet(selected ?? null);
     overrideSpeedFactor();
+    setCameraState('ZOOMED_IN');
   };
+
+  const isDisabled = cameraState === 'INTRO_ANIMATION';
 
   return (
     <div className="absolute bottom-5 left-5 right-5">
       <div className='flex gap-2 justify-center'>
         {planets.map((planet) => (
           <Button 
+            isDisabled={isDisabled}
             key={planet.name} 
             variant='flat' 
             color='secondary' 
