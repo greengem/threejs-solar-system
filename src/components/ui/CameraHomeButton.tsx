5664466bb0998c079f2eaedcfb3d2ae9bd828ac9
Green Gem <chris_waitt@me.com>
2023-11-07 17:23:13 +0000

    Home button allows user to exit detil view too

--- src/components/ui/CameraHomeButton.tsx
+++ src/components/ui/CameraHomeButton.tsx
@@ -1,18 +1,26 @@
 // CameraHomeButton.tsx
 import { useCameraContext } from '../../contexts/CameraContext';
+import { useSelectedPlanet } from '../../contexts/SelectedPlanetContext';
+import { useSpeedControl } from '../../contexts/SpeedControlContext';
 import { Button } from '@nextui-org/react';
 
 const CameraHomeButton = () => {
   const { cameraState, setCameraState } = useCameraContext();
+  const { restoreSpeedFactor } = useSpeedControl();
+  const [selectedPlanet, setSelectedPlanet] = useSelectedPlanet();
 
   const moveToHome = () => {
     setCameraState('MOVING_TO_HOME');
+    if(selectedPlanet) {
+      setSelectedPlanet(null)
+      restoreSpeedFactor();
+    }
   };
 
-  const isDisabled = cameraState !== 'FREE';
+  const isButtonDisabled = cameraState === 'INTRO_ANIMATION';
 
   return (
-    <Button color='secondary' className='absolute right-5 top-20' disabled={isDisabled} onClick={moveToHome}>
+    <Button color='secondary' className='absolute right-5 top-20' isDisabled={isButtonDisabled} onClick={moveToHome}>
       Home
     </Button>
   );
