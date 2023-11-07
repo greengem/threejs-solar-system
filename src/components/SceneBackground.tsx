03da0d773f63c4e2e059cf831c79ff117b08a49d
Green Gem <chris_waitt@me.com>
2023-11-07 13:03:12 +0000

    Correctly implement intro animation, note the lerp will need some tweaks when we change camera state lerp by nature will go very slow at the end so we need to make sure there isnt a jump at the end of the animation

--- src/components/SceneBackground.tsx
+++ src/components/SceneBackground.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
-import { useLoader, useThree } from '@react-three/fiber';
-import { TextureLoader, EquirectangularReflectionMapping } from 'three';
+import React, { useEffect, useRef } from 'react';
+import { useLoader, useThree, useFrame } from '@react-three/fiber';
+import { TextureLoader, EquirectangularReflectionMapping, Vector3 } from 'three';
+import { useCameraContext } from '../contexts/CameraContext'; // make sure to import your camera context
 
 type SceneBackgroundProps = {
   texturePath: string;
@@ -8,15 +9,19 @@ type SceneBackgroundProps = {
 
 const SceneBackground: React.FC<SceneBackgroundProps> = ({ texturePath }) => {
   const { scene, camera } = useThree();
+  const { setCameraState } = useCameraContext();
+  const introAnimationCompleted = useRef(false);
 
   useEffect(() => {
-    camera.position.set(-40, 40, 40);
+    // Set initial camera position for the intro animation
+    camera.position.set(-150, 0, 150);
     camera.lookAt(0, 0, 0);
   }, [camera]);
 
   const texture = useLoader(TextureLoader, texturePath);
 
   useEffect(() => {
+    // Set the background texture
     const prevBackground = scene.background;
     texture.mapping = EquirectangularReflectionMapping;
     scene.background = texture;
@@ -25,6 +30,21 @@ const SceneBackground: React.FC<SceneBackgroundProps> = ({ texturePath }) => {
       scene.background = prevBackground;
     };
   }, [texture, scene]);
+
+  useFrame(() => {
+    if (!introAnimationCompleted.current) {
+      // Define the target position
+      const targetPosition = new Vector3(7, 6, 7);
+      // Update the camera position to move towards the target position
+      camera.position.lerp(targetPosition, 0.01);
+      // When the camera is close enough to the target, end the intro animation
+      if (camera.position.distanceTo(targetPosition) < 0.05) {
+        introAnimationCompleted.current = true;
+        camera.position.copy(targetPosition); // Snap to the exact position
+        setCameraState('FREE'); // Update the camera state
+      }
+    }
+  });
 
   return null;
 };
