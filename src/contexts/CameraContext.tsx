39d70cf7dcb7fddca6a9f67f178b42b22d102813
Green Gem <chris_waitt@me.com>
2023-11-07 13:00:10 +0000

    New context to set the state of the camera, this should make handling the camera controls a lot easier

---
+++ src/contexts/CameraContext.tsx
@@ -0,0 +1,33 @@
+// CameraContext.tsx
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+type CameraState = 'FREE' | 'INTRO_ANIMATION' | 'ZOOMED_IN' | 'MOVING_TO_HOME';
+
+interface CameraContextType {
+  cameraState: CameraState;
+  setCameraState: (state: CameraState) => void;
+}
+
+const CameraContext = createContext<CameraContextType | null>(null);
+
+interface CameraProviderProps {
+  children: ReactNode;
+}
+
+export const CameraProvider: React.FC<CameraProviderProps> = ({ children }) => {
+  const [cameraState, setCameraState] = useState<CameraState>('INTRO_ANIMATION');
+
+  return (
+    <CameraContext.Provider value={{ cameraState, setCameraState }}>
+      {children}
+    </CameraContext.Provider>
+  );
+};
+
+export const useCameraContext = () => {
+  const context = useContext(CameraContext);
+  if (!context) {
+    throw new Error('useCameraContext must be used within a CameraProvider');
+  }
+  return context;
+};
