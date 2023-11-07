1da715b6ea19bb2d9e51278df5df264a217e9b64
Green Gem <chris_waitt@me.com>
2023-11-07 16:04:42 +0000

    Added cases for each camera state with lerp for smooth animation. we also set moving home to free once the destination is reached

--- src/components/CameraController.tsx
+++ src/components/CameraController.tsx
@@ -1,44 +1,57 @@
 import React, { useRef } from 'react';
 import { useThree, useFrame } from '@react-three/fiber';
-import { Vector3 } from 'three';
+import {  Vector3 } from 'three';
 import { OrbitControls as DreiOrbitControls } from '@react-three/drei';
 import { useSelectedPlanet } from '../contexts/SelectedPlanetContext';
 import { usePlanetPositions } from '../contexts/PlanetPositionsContext';
+import { useCameraContext } from '../contexts/CameraContext';
 
 const CameraController: React.FC = () => {
-  const orbitControlsRef = useRef<any>(null); //FIX ANY TYPE
+  const orbitControlsRef = useRef<any>(null); // FIX ANY TYPE 
   const { camera } = useThree();
   const [selectedPlanet] = useSelectedPlanet();
   const { planetPositions } = usePlanetPositions();
+  const { cameraState, setCameraState } = useCameraContext();
+  const homePosition = new Vector3(7, 6, 7);
+  const lerpFactor = 0.05;
 
   useFrame(() => {
     const controls = orbitControlsRef.current;
-    const lerpTarget = new Vector3();
-    
-    if (selectedPlanet) {
-      const currentPlanetPosition = planetPositions[selectedPlanet.name];
-  
-      if (currentPlanetPosition) {
-        const planetPosition = new Vector3(...currentPlanetPosition);
-  
+
+    switch (cameraState) {
+      case 'FREE':
+        if (controls) controls.enabled = true;
+        break;
+
+        case 'ZOOMED_IN':
+          if (selectedPlanet) {
+            const currentPlanetPosition = planetPositions[selectedPlanet.name];
+            if (currentPlanetPosition && controls) {
+              controls.enabled = false;
+              const planetPosition = new Vector3(...currentPlanetPosition);
+
+              const targetCameraPosition = planetPosition.clone().add(new Vector3(1, 0.5, 0).multiplyScalar(selectedPlanet.radius * 3));
+              camera.position.lerp(targetCameraPosition, lerpFactor);
+              camera.lookAt(planetPosition);
+            }
+          }
+          break;
+        
+
+      case 'MOVING_TO_HOME':
         if (controls) controls.enabled = false;
-  
-        lerpTarget.copy(planetPosition).add(new Vector3(1, 0.5, 0).multiplyScalar(selectedPlanet.radius * 3));
-        //camera.position.lerp(lerpTarget, 0.01);
-        camera.lookAt(planetPosition);
-      }
-    } else {
-      //const remotePosition = new Vector3(7, 4, 7);
-  
-      //lerpTarget.copy(remotePosition);
-      //camera.position.lerp(lerpTarget, 0.01);
+        camera.position.lerp(homePosition, lerpFactor);
+        camera.lookAt(new Vector3(0, 0, 0));
+        if (camera.position.distanceTo(homePosition) < 0.1) {
+          camera.position.copy(homePosition);
+          setCameraState('FREE');
+        }
+        break;
       
-      if (controls) controls.enabled = true;
     }
-  
+
     camera.updateProjectionMatrix();
   });
-  
 
   return (
     <DreiOrbitControls
