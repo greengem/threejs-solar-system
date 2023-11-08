aa4b1a05786c06a01338d372a9b484fd11026a91
Green Gem <chris_waitt@me.com>
2023-11-08 15:11:37 +0000

    Since @react-three/drei isnt exporting types for Orbit controls the best solution i found was to import orbit controls from three-stdlib and use those. Also added a new case for our detail view

--- src/components/motion/CameraController.tsx
+++ src/components/motion/CameraController.tsx
@@ -2,12 +2,14 @@ import React, { useRef } from 'react';
 import { useThree, useFrame } from '@react-three/fiber';
 import {  Vector3 } from 'three';
 import { OrbitControls as DreiOrbitControls } from '@react-three/drei';
+import { OrbitControls } from 'three-stdlib';
 import { useSelectedPlanet } from '../../contexts/SelectedPlanetContext';
 import { usePlanetPositions } from '../../contexts/PlanetPositionsContext';
 import { useCameraContext } from '../../contexts/CameraContext';
 
+
 const CameraController: React.FC = () => {
-  const orbitControlsRef = useRef<any>(null); // FIX ANY TYPE 
+  const orbitControlsRef = useRef<OrbitControls>(null);
   const { camera } = useThree();
   const [selectedPlanet] = useSelectedPlanet();
   const { planetPositions } = usePlanetPositions();
@@ -19,25 +21,13 @@ const CameraController: React.FC = () => {
     const controls = orbitControlsRef.current;
 
     switch (cameraState) {
+      
+      // Free is our default camera that lets the user move around freely.
       case 'FREE':
         if (controls) controls.enabled = true;
         break;
 
-        case 'ZOOMED_IN':
-          if (selectedPlanet) {
-            const currentPlanetPosition = planetPositions[selectedPlanet.name];
-            if (currentPlanetPosition && controls) {
-              controls.enabled = false;
-              const planetPosition = new Vector3(...currentPlanetPosition);
-
-              const targetCameraPosition = planetPosition.clone().add(new Vector3(1, 0.5, 0).multiplyScalar(selectedPlanet.radius * 3));
-              camera.position.lerp(targetCameraPosition, lerpFactor);
-              camera.lookAt(planetPosition);
-            }
-          }
-          break;
-        
-
+      // Move the camera back to the default position and look at the sun.
       case 'MOVING_TO_HOME':
         if (controls) controls.enabled = false;
         camera.position.lerp(homePosition, lerpFactor);
@@ -47,7 +37,30 @@ const CameraController: React.FC = () => {
           setCameraState('FREE');
         }
         break;
-      
+
+      // Animation to handle moving from current position to a planet
+      case 'ZOOMING_IN':
+        if (selectedPlanet) {
+          const currentPlanetPosition = planetPositions[selectedPlanet.name];
+          if (currentPlanetPosition && controls) {
+            controls.enabled = false;
+            const planetPosition = new Vector3(...currentPlanetPosition);
+            const targetCameraPosition = planetPosition.clone().add(new Vector3(1, 0.5, 0).multiplyScalar(selectedPlanet.radius * 3));
+            camera.position.lerp(targetCameraPosition, lerpFactor);
+            camera.lookAt(planetPosition);
+
+            if (camera.position.distanceTo(targetCameraPosition) < selectedPlanet.radius * 0.1) {
+              setCameraState('DETAIL_VIEW');
+            }
+          }
+        }
+        break;
+
+      // After zooming in we stop the camera while looking at the planet. Ideally we could lock the zoom factor but allow the user to rotate around the planet
+      case 'DETAIL_VIEW':
+        if (controls) controls.enabled = true;
+        break;
+
     }
 
     camera.updateProjectionMatrix();
