98e579ad09bce0f9e67bc56ed8e88df124e0ac0d
Green Gem <chris_waitt@me.com>
2023-11-08 11:12:46 +0000

    Speed up intro animation and smooth transition

--- src/components/SceneBackground.tsx
+++ src/components/SceneBackground.tsx
@@ -32,8 +32,8 @@ const SceneBackground: React.FC<SceneBackgroundProps> = ({ texturePath }) => {
   useFrame(() => {
     if (!introAnimationCompleted.current) {
       const targetPosition = new Vector3(7, 6, 7);
-      camera.position.lerp(targetPosition, 0.01);
-      if (camera.position.distanceTo(targetPosition) < 0.1) {
+      camera.position.lerp(targetPosition, 0.03);
+      if (camera.position.distanceTo(targetPosition) < 0.01) {
         introAnimationCompleted.current = true;
         camera.position.copy(targetPosition);
         setCameraState('FREE');
