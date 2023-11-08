9afa4a8496c6d50211833a4deaab4139580a4ce3
Green Gem <chris_waitt@me.com>
2023-11-08 15:10:13 +0000

    Only show details when we are in detail view. this needs adjustment

--- src/components/ui/PlanetDetail.tsx
+++ src/components/ui/PlanetDetail.tsx
@@ -1,28 +1,49 @@
-// PlanetDetail.tsx
+import { motion, AnimatePresence } from 'framer-motion';
 import {Tabs, Tab} from "@nextui-org/tabs";
 import { useSelectedPlanet } from '../../contexts/SelectedPlanetContext';
 
-const PlanetDetail: React.FC = () => {
+const PlanetDetail: React.FC<{ visible: boolean }> = ({ visible }) => {
   const [selectedPlanet] = useSelectedPlanet();
 
-  if (!selectedPlanet) return null;
+  // Define your animation variants
+  const variants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: 10 }
+  };
+  
+  
 
   return (
-    <div className='absolute left-5 right-5 top-30 w-[400px]'>
-        <h1 className='
-          text-white
-          mb-5 
-          tracking-tight font-semibold 
-          text-5xl lg:text-7xl xl:text-8xl 
-        '>
-          {selectedPlanet.name}
-        </h1>
-        <Tabs aria-label="Planet detail tabs" color="secondary">
-          <Tab key="details" title="Details"><p className="text-sm">{selectedPlanet.description}</p></Tab>
-          <Tab key="stats" title="Stats"></Tab>
-          <Tab key="photos" title="Photos"></Tab>
-        </Tabs>
-    </div>
+    <AnimatePresence>
+      {visible && selectedPlanet && (
+        <motion.div
+          className='absolute left-5 right-5 top-30 w-[400px]'
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          variants={variants}
+          transition={{ duration: 1, ease: "easeOut" }}
+
+        >
+          <h1 className='
+            text-white
+            mb-5 
+            tracking-tight font-semibold 
+            text-5xl lg:text-7xl xl:text-8xl 
+          '>
+            {selectedPlanet.name}
+          </h1>
+          <Tabs aria-label="Planet detail tabs" color="secondary">
+            <Tab key="details" title="Details">
+              <p className="text-sm">{selectedPlanet.description}</p>
+            </Tab>
+            <Tab key="stats" title="Stats"></Tab>
+            <Tab key="photos" title="Photos"></Tab>
+          </Tabs>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
