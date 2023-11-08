d9041a05ae18de072c8cf351b64d09a2992cbd67
Green Gem <chris_waitt@me.com>
2023-11-08 16:15:26 +0000

    Refactor Camera controls so Vector3 isnt being used every frame

--- src/components/motion/CameraController.tsx
+++ src/components/motion/CameraController.tsx
@@ -1,69 +1,69 @@
 import React, { useRef } from 'react';
 import { useThree, useFrame } from '@react-three/fiber';
-import {  Vector3 } from 'three';
+import { Vector3 } from 'three';
 import { OrbitControls as DreiOrbitControls } from '@react-three/drei';
 import { OrbitControls } from 'three-stdlib';
 import { useSelectedPlanet } from '../../contexts/SelectedPlanetContext';
 import { usePlanetPositions } from '../../contexts/PlanetPositionsContext';
 import { useCameraContext } from '../../contexts/CameraContext';
-
+import { useCameraSetup } from '../../hooks/useCameraSetup';
+import { useIntroAnimation } from '../../hooks/useIntroAnimation';
 
 const CameraController: React.FC = () => {
+  useCameraSetup();
+  useIntroAnimation();
+  
   const orbitControlsRef = useRef<OrbitControls>(null);
   const { camera } = useThree();
   const [selectedPlanet] = useSelectedPlanet();
   const { planetPositions } = usePlanetPositions();
   const { cameraState, setCameraState } = useCameraContext();
-  const homePosition = new Vector3(7, 6, 7);
+
+  const homePosition = useRef(new Vector3(7, 6, 7)).current;
+  const lookAtSun = useRef(new Vector3(0, 0, 0)).current;
   const lerpFactor = 0.05;
+  const cameraPositionEpsilon = 0.1;
 
   useFrame(() => {
     const controls = orbitControlsRef.current;
 
-    switch (cameraState) {
-      
-      // Free is our default camera that lets the user move around freely.
-      case 'FREE':
-        if (controls) controls.enabled = true;
-        break;
+    if (controls) {
+      switch (cameraState) {
 
-      // Move the camera back to the default position and look at the sun.
-      case 'MOVING_TO_HOME':
-        if (controls) controls.enabled = false;
-        camera.position.lerp(homePosition, lerpFactor);
-        camera.lookAt(new Vector3(0, 0, 0));
-        if (camera.position.distanceTo(homePosition) < 0.1) {
-          camera.position.copy(homePosition);
-          setCameraState('FREE');
-        }
-        break;
+        case 'FREE':
+          controls.enabled = true;
+          break;
 
-      // Animation to handle moving from current position to a planet
-      case 'ZOOMING_IN':
-        if (selectedPlanet) {
-          const currentPlanetPosition = planetPositions[selectedPlanet.name];
-          if (currentPlanetPosition && controls) {
-            controls.enabled = false;
-            const planetPosition = new Vector3(...currentPlanetPosition);
-            const targetCameraPosition = planetPosition.clone().add(new Vector3(1, 0.5, 0).multiplyScalar(selectedPlanet.radius * 3));
-            camera.position.lerp(targetCameraPosition, lerpFactor);
-            camera.lookAt(planetPosition);
+        case 'MOVING_TO_HOME':
+          controls.enabled = false;
+          camera.position.lerp(homePosition, lerpFactor);
+          camera.lookAt(lookAtSun);
+          if (camera.position.distanceTo(homePosition) < cameraPositionEpsilon) {
+            camera.position.copy(homePosition);
+            setCameraState('FREE');
+          }
+          break;
 
-            if (camera.position.distanceTo(targetCameraPosition) < selectedPlanet.radius * 0.1) {
-              setCameraState('DETAIL_VIEW');
+        case 'ZOOMING_IN':
+          if (selectedPlanet) {
+            const currentPlanetPosition = planetPositions[selectedPlanet.name];
+            if (currentPlanetPosition) {
+              controls.enabled = false;
+              const planetPosition = new Vector3(...currentPlanetPosition);
+              const targetCameraPosition = planetPosition.clone().add(new Vector3(1, 0.5, 0).multiplyScalar(selectedPlanet.radius * 3));
+              camera.position.lerp(targetCameraPosition, lerpFactor);
+              camera.lookAt(planetPosition);
+
+              if (camera.position.distanceTo(targetCameraPosition) < selectedPlanet.radius * cameraPositionEpsilon) {
+                setCameraState('DETAIL_VIEW');
+              }
             }
           }
-        }
-        break;
-
-      // After zooming in we stop the camera while looking at the planet. Ideally we could lock the zoom factor but allow the user to rotate around the planet
-      case 'DETAIL_VIEW':
-        if (controls) controls.enabled = true;
-        break;
-
+          break;
+          
+      }
+      camera.updateProjectionMatrix();
     }
-
-    camera.updateProjectionMatrix();
   });
 
   return (
