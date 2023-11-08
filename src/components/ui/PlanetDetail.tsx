d5142fb52e5ac78e5f15438753787408f8d88076
Green Gem <chris_waitt@me.com>
2023-11-08 11:14:28 +0000

    added tabs to detail view

--- src/components/ui/PlanetDetail.tsx
+++ src/components/ui/PlanetDetail.tsx
@@ -1,24 +1,14 @@
 // PlanetDetail.tsx
-import { Button } from '@nextui-org/button';
+import {Tabs, Tab} from "@nextui-org/tabs";
 import { useSelectedPlanet } from '../../contexts/SelectedPlanetContext';
-import { useSpeedControl } from '../../contexts/SpeedControlContext';
-import { useCameraContext } from '../../contexts/CameraContext';
 
 const PlanetDetail: React.FC = () => {
-  const [selectedPlanet, setSelectedPlanet] = useSelectedPlanet();
-  const { restoreSpeedFactor } = useSpeedControl();
-  const { setCameraState } = useCameraContext();
-
-  const handleExitDetailMode = () => {
-    setSelectedPlanet(null);
-    restoreSpeedFactor();
-    setCameraState('MOVING_TO_HOME');
-  };
+  const [selectedPlanet] = useSelectedPlanet();
 
   if (!selectedPlanet) return null;
 
   return (
-    <div className='absolute left-5 right-5 top-5 w-[400px]'>
+    <div className='absolute left-5 right-5 top-30 w-[400px]'>
         <h1 className='
           text-white
           mb-5 
@@ -27,17 +17,11 @@ const PlanetDetail: React.FC = () => {
         '>
           {selectedPlanet.name}
         </h1>
-        <p className='
-          hidden md:block
-          text-white 
-          mb-5 ml-1
-          text-xs md:text-sm
-        '>
-          {selectedPlanet.description}
-        </p>
-        <Button size='sm' className='ml-1 hidden md:block' color='secondary' onClick={handleExitDetailMode}>
-          Exit Detail View
-        </Button>
+        <Tabs aria-label="Planet detail tabs" color="secondary">
+          <Tab key="details" title="Details"><p className="text-sm">{selectedPlanet.description}</p></Tab>
+          <Tab key="stats" title="Stats"></Tab>
+          <Tab key="photos" title="Photos"></Tab>
+        </Tabs>
     </div>
   );
 };
