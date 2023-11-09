2921e86594f48b1ed4e2cd6a81874aa4d6283adc
Green Gem <chris_waitt@me.com>
2023-11-09 16:24:56 +0000

    fixed planet rotation

--- src/components/celestial/Planets.tsx
+++ src/components/celestial/Planets.tsx
@@ -10,7 +10,7 @@ import { Mesh } from 'three';
 type ExtendedPlanetData = PlanetData & { orbitProgress: number };
 
 const Planet: React.FC<ExtendedPlanetData> = ({
-  name, 
+  name,
   texturePath,
   position,
   radius,
@@ -28,18 +28,24 @@ const Planet: React.FC<ExtendedPlanetData> = ({
 
   useFrame(() => {
     if (ref.current) {
-      ref.current.rotation.y += rotationSpeed / 100;
-      ref.current.rotation.x = tilt;
+      // Assuming rotationSpeed is given in degrees per second,
+      // and useFrame runs at approximately 60 frames per second.
+      // Convert rotationSpeed to radians per second (radians = degrees * (Math.PI / 180))
+      // Then divide by 60 to get the rotation per frame
+      const rotationPerFrame = (rotationSpeed * (Math.PI / 180)) / 60;
+      ref.current.rotation.y += rotationPerFrame;
     }
   });
+  
 
   useEffect(() => {
     setPlanetPosition(name, [x, 0, z]);
   }, [x, z, name, setPlanetPosition]);
 
+  // Ensure the ref is assigned to the mesh component
   return (
     <>
-      <mesh position={[x, 0, z]} rotation={[tilt, 0, 0]}>
+      <mesh ref={ref} position={[x, 0, z]} rotation={[tilt, 0, 0]}>
         <Sphere args={sphereArgs}>
           <meshStandardMaterial map={texture} />
         </Sphere>
