45b6d15c030459b158c8bcf5689b5f415bead902
Green Gem <chris_waitt@me.com>
2023-11-09 16:24:42 +0000

    Hide fine details on mobile.

--- src/components/ui/PlanetDetail.tsx
+++ src/components/ui/PlanetDetail.tsx
@@ -37,71 +37,69 @@ const PlanetDetail: React.FC = () => {
           <h1 className='tracking-tight font-semibold text-7xl lg:text-8xl xl:text-8xl opacity-90'>
             {displayedPlanet ? displayedPlanet.name : ''}
           </h1>
-          <h4 className='tracking-tight text-2xl mb-5 ml-1'>{displayedPlanet?.displayStats.classification}</h4>
-          <div>
-            <ul className='text-sm w-64 ml-1'>
-              <li>
-                <p>
-                  <span className='text-secondary'>Orbital Periood: </span>
-                  <span>{displayedPlanet?.displayStats.orbitalPeriod}</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className='text-secondary'>Mean Distance from Sun: </span>
-                  <span>{displayedPlanet?.displayStats.meanDistanceFromSun}</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className='text-secondary'>Radius: </span>
-                  <span>{displayedPlanet?.displayStats.accurateRadius}</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className='text-secondary'>Mass: </span>
-                  <span>{displayedPlanet?.displayStats.mass}</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className='text-secondary'>Surface Gravity: </span>
-                  <span>{displayedPlanet?.displayStats.surfaceGravity}</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className='text-secondary'>Rotation Period: </span>
-                  <span>{displayedPlanet?.displayStats.rotationPeriod}</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className='text-secondary'>Axial Tilt: </span>
-                  <span>{displayedPlanet?.displayStats.axialTilt}</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className='text-secondary'>Number of Moons: </span>
-                  <span>{displayedPlanet?.displayStats.numberOfMoons}</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className='text-secondary'>Atmospheric Composition: </span>
-                  <span>{displayedPlanet?.displayStats.atmosphericComposition}</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className='text-secondary'>Surface Tempoerature: </span>
-                  <span>{displayedPlanet?.displayStats.surfaceTemp}</span>
-                </p>
-              </li>
-            </ul>
-          </div>
+          <h4 className='tracking-tight text-2xl mb-5 ml-1 text-secondary font-semibold'>{displayedPlanet?.displayStats.classification}</h4>
+          <ul className='text-sm w-64 ml-2 hidden lg:block text-gray-200'>
+            <li>
+              <p>
+                <span className='font-semibold'>Orbital Periood: </span>
+                <span>{displayedPlanet?.displayStats.orbitalPeriod} Earth days</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className='font-semibold'>Mean Distance from Sun: </span>
+                <span>{displayedPlanet?.displayStats.meanDistanceFromSun} AU</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className='font-semibold'>Radius: </span>
+                <span>{displayedPlanet?.displayStats.accurateRadius} km</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className='font-semibold'>Mass: </span>
+                <span>{displayedPlanet?.displayStats.mass} Earth masses</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className='font-semibold'>Surface Gravity: </span>
+                <span>{displayedPlanet?.displayStats.surfaceGravity} g</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className='font-semibold'>Rotation Period: </span>
+                <span>{displayedPlanet?.displayStats.rotationPeriod} Earth days</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className='font-semibold'>Axial Tilt: </span>
+                <span>{displayedPlanet?.displayStats.axialTilt}°</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className='font-semibold'>Number of Moons: </span>
+                <span>{displayedPlanet?.displayStats.numberOfMoons}</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className='font-semibold'>Atmospheric Composition: </span>
+                <span>{displayedPlanet?.displayStats.atmosphericComposition}</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span className='font-semibold'>Surface Tempoerature: </span>
+                <span>{displayedPlanet?.displayStats.surfaceTemp}</span>
+              </p>
+            </li>
+          </ul>
         </motion.div>
       )}
     </AnimatePresence>
