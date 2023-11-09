777ca361a2a999a3a15f926189edc4872610c685
Green Gem <chris_waitt@me.com>
2023-11-09 11:04:07 +0000

    Planet menu now disabling button for active planet and hidden with framer motion during intro animation

--- src/components/ui/PlanetMenu.tsx
+++ src/components/ui/PlanetMenu.tsx
@@ -1,5 +1,6 @@
 // PlanetMenu.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 import { useSpeedControl } from '../../contexts/SpeedControlContext';
 import { useSelectedPlanet } from '../../contexts/SelectedPlanetContext';
 import { useCameraContext } from '../../contexts/CameraContext';
@@ -11,9 +12,16 @@ interface PlanetMenuProps {
 }
 
 const PlanetMenu: React.FC<PlanetMenuProps> = ({ planets }) => {
-  const [, setSelectedPlanet] = useSelectedPlanet();
+  const [selectedPlanet, setSelectedPlanet] = useSelectedPlanet();
   const { overrideSpeedFactor } = useSpeedControl();
   const { cameraState, setCameraState } = useCameraContext();
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (cameraState === 'FREE') {
+      controls.start({ y: 0, opacity: 1 });
+    }
+  }, [cameraState, controls]);
 
   const handleSelect = (planetName: string) => {
     const selected = planets.find((planet) => planet.name === planetName);
@@ -22,25 +30,34 @@ const PlanetMenu: React.FC<PlanetMenuProps> = ({ planets }) => {
     setCameraState('ZOOMING_IN');
   };
 
-  const isDisabled = cameraState === 'INTRO_ANIMATION';
+  const menuVariants = {
+    hidden: { y: '170%', opacity: 1 },
+    visible: { y: 0, opacity: 1, transition: { type: 'spring', stiffness: 100 } }
+  };
+
 
   return (
-    <div className="absolute bottom-5 left-5 right-5">
+    <motion.div 
+      className="fixed bottom-5 left-5 right-5"
+      variants={menuVariants}
+      initial="hidden"
+      animate={controls}
+    >
       <div className='flex flex-wrap gap-2 justify-center'>
         {planets.map((planet) => (
           <Button 
-            isDisabled={isDisabled}
-            key={planet.name} 
+            key={planet.id} 
             variant='flat' 
             color='secondary' 
             size='sm' 
             onClick={() => handleSelect(planet.name)}
+            isDisabled={selectedPlanet?.id === planet.id}
           >
             {planet.name}
           </Button>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
