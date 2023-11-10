6ac0165d9350f74f95443f9734def8f084ec7025
Green Gem <chris_waitt@me.com>
2023-11-10 11:57:27 +0000

    Tweaked intro text to accomodate now lerp factor

--- src/components/ui/IntroText.tsx
+++ src/components/ui/IntroText.tsx
@@ -3,26 +3,22 @@ import { motion } from 'framer-motion';
 
 const IntroText: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const [display, setDisplay] = useState(true); // New state to control display
+  const [display, setDisplay] = useState(true);
 
   useEffect(() => {
-    // Set a timeout to fade in the text
     const fadeInTimeoutId = setTimeout(() => {
       setIsVisible(true);
-    }, 4000); // Fade in after 4 seconds
+    }, 3000);
 
-    // Set another timeout to fade out the text after a certain time
     const fadeOutTimeoutId = setTimeout(() => {
       setIsVisible(false);
-    }, 14000); // Fade out after 10 seconds (total 14 seconds from the initial render)
+    }, 8000);
 
-    // Set a timeout to hide the text after the fade out
     const hideTimeoutId = setTimeout(() => {
-      setDisplay(false); // Hide the text by not rendering the component
-    }, 14000); // Hide after 1 second of fade out animation
+      setDisplay(false);
+    }, 9200);
 
     return () => {
-      // Clear timeouts if the component unmounts
       clearTimeout(fadeInTimeoutId);
       clearTimeout(fadeOutTimeoutId);
       clearTimeout(hideTimeoutId);
@@ -30,14 +26,14 @@ const IntroText: React.FC = () => {
   }, []);
 
   if (!display) {
-    return null; // Don't render the component if display is false
+    return null;
   }
 
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: isVisible ? 1 : 0 }}
-      transition={{ duration: 1 }} // Duration for the fade in/out transition
+      transition={{ duration: 1 }}
       className="
         absolute
         top-0 left-0 bottom-0 right-0
