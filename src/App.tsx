ce548dd7f89e32717adfbae11eb40c32805220db
Green Gem <chris_waitt@me.com>
2024-10-23 08:13:43 +0100

    Refactor App component import paths and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 // App.tsx
-import SolarSystem from './components/SolarSystem';
-import Providers from './Providers';
+import SolarSystem from "./components/SolarSystem";
+import Providers from "./Providers";
 
-function App() {
+export default function App() {
   return (
     <Providers>
-      <div className='w-screen h-screen overflow-hidden'>
+      <div className="w-screen h-screen overflow-hidden">
         <SolarSystem />
       </div>
     </Providers>
   );
 }
-
-export default App;
