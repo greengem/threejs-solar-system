b644a088a2bc65c130c0216108fdfdcabc6c4f0f
Green Gem <chris_waitt@me.com>
2024-10-23 08:13:48 +0100

    Refactor import paths and formatting in Providers.tsx

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,29 +1,21 @@
 // Providers.tsx
-import { ReactNode } from 'react';
-import { SelectedPlanetProvider } from './contexts/SelectedPlanetContext';
-import { SpeedControlProvider } from './contexts/SpeedControlContext';
-import { PlanetPositionsProvider } from './contexts/PlanetPositionsContext';
-import { CameraProvider } from './contexts/CameraContext';
+import { ReactNode } from "react";
+import { SelectedPlanetProvider } from "./contexts/SelectedPlanetContext";
+import { SpeedControlProvider } from "./contexts/SpeedControlContext";
+import { PlanetPositionsProvider } from "./contexts/PlanetPositionsContext";
+import { CameraProvider } from "./contexts/CameraContext";
 import { NextUIProvider } from "@nextui-org/react";
 
-type ProvidersProps = {
-    children: ReactNode;
-  };
-
-const Providers: React.FC<ProvidersProps> = ({ children }) => {
+export default function Providers({ children }: { children: ReactNode }) {
   return (
     <NextUIProvider>
       <SelectedPlanetProvider>
         <SpeedControlProvider>
           <PlanetPositionsProvider>
-            <CameraProvider>
-                {children}
-            </CameraProvider>
+            <CameraProvider>{children}</CameraProvider>
           </PlanetPositionsProvider>
         </SpeedControlProvider>
       </SelectedPlanetProvider>
     </NextUIProvider>
   );
-};
-
-export default Providers;
+}
