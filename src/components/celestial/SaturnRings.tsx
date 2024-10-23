8510dc8c1f7abaf821802cd41ddb01d70ef31a27
Green Gem <chris_waitt@me.com>
2024-10-23 08:15:17 +0100

    Refactor import paths and formatting in SaturnRings.tsx

--- src/components/celestial/SaturnRings.tsx
+++ src/components/celestial/SaturnRings.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { useLoader } from '@react-three/fiber';
-import { TextureLoader, DoubleSide } from 'three';
-import { Ring } from '@react-three/drei';
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader, DoubleSide } from "three";
+import { Ring } from "@react-three/drei";
 
 type SaturnRingsProps = {
   texturePath: string;
@@ -9,14 +8,23 @@ type SaturnRingsProps = {
   outerRadius: number;
 };
 
-const SaturnRings: React.FC<SaturnRingsProps> = ({ texturePath, innerRadius, outerRadius }) => {
+export default function SaturnRings({
+  texturePath,
+  innerRadius,
+  outerRadius,
+}: SaturnRingsProps) {
   const texture = useLoader(TextureLoader, texturePath);
 
   return (
-    <Ring args={[innerRadius, outerRadius, 128]} rotation={[-Math.PI / 2, 0, 0]}>
-      <meshStandardMaterial map={texture} side={DoubleSide} transparent={true} />
+    <Ring
+      args={[innerRadius, outerRadius, 128]}
+      rotation={[-Math.PI / 2, 0, 0]}
+    >
+      <meshStandardMaterial
+        map={texture}
+        side={DoubleSide}
+        transparent={true}
+      />
     </Ring>
   );
-};
-
-export default SaturnRings;
+}
