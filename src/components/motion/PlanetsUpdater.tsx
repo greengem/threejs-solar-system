566a299d3c037f37b38bab40b160e8598e0b21d3
Green Gem <chris_waitt@me.com>
2024-10-23 08:15:29 +0100

    Refactor import paths and formatting in PlanetsUpdater.tsx

--- src/components/motion/PlanetsUpdater.tsx
+++ src/components/motion/PlanetsUpdater.tsx
@@ -1,17 +1,22 @@
 // PlanetsUpdater.tsx
-import React, { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { PlanetData } from '../../../types';
-import { useSpeedControl } from '../../contexts/SpeedControlContext';
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { PlanetData } from "../../../types";
+import { useSpeedControl } from "../../contexts/SpeedControlContext";
 
 type PlanetsUpdaterProps = {
-  setPlanetOrbitProgress: React.Dispatch<React.SetStateAction<{ [key: string]: number }>>;
+  setPlanetOrbitProgress: React.Dispatch<
+    React.SetStateAction<{ [key: string]: number }>
+  >;
   planets: PlanetData[];
 };
 
 const ORBIT_SPEED_FACTOR = 50;
 
-const PlanetsUpdater: React.FC<PlanetsUpdaterProps> = ({ setPlanetOrbitProgress, planets }) => {
+export default function PlanetsUpdater({
+  setPlanetOrbitProgress,
+  planets,
+}: PlanetsUpdaterProps) {
   const { speedFactor } = useSpeedControl();
   const lastElapsedTimeRef = useRef(0);
 
@@ -21,15 +26,21 @@ const PlanetsUpdater: React.FC<PlanetsUpdaterProps> = ({ setPlanetOrbitProgress,
     lastElapsedTimeRef.current = elapsedTime;
 
     setPlanetOrbitProgress((prevOrbitProgress) => {
-      return planets.reduce((acc, planet) => {
-        const orbitSpeedRadians = (((planet.orbitSpeed * ORBIT_SPEED_FACTOR) / 360) * (2 * Math.PI)) * speedFactor;
-        acc[planet.name] = (prevOrbitProgress[planet.name] || 0) + orbitSpeedRadians * deltaTime;
-        return acc;
-      }, { ...prevOrbitProgress });
+      return planets.reduce(
+        (acc, planet) => {
+          const orbitSpeedRadians =
+            ((planet.orbitSpeed * ORBIT_SPEED_FACTOR) / 360) *
+            (2 * Math.PI) *
+            speedFactor;
+          acc[planet.name] =
+            (prevOrbitProgress[planet.name] || 0) +
+            orbitSpeedRadians * deltaTime;
+          return acc;
+        },
+        { ...prevOrbitProgress }
+      );
     });
   });
 
   return null;
-};
-
-export default PlanetsUpdater;
+}
